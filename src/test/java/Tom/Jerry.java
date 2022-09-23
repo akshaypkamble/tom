2123ef6760d6f725c031349833ce9b9c13bc192f
akshay <akshaypkamble393@gmail.com>
2022-09-23 18:46:03 +0530

    tom and jerry

---
+++ src/test/java/Tom/Jerry.java
@@ -0,0 +1,17 @@
+package Tom;
+
+import org.testng.annotations.Test;
+
+public class Jerry {
+
+	
+	@Test
+	public void contact()
+	{
+		System.out.println("This is my first commit - MavenProjects");
+		
+		System.out.println("ok");
+		
+		System.out.println("jerry");
+	}
+}
